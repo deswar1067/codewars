7fc97f1f56d3c2c335302aeb9a9e3ddf6f24400f
deswar1067 <deswar1067@tut.by>
2022-07-06 21:27:52 +0300

    06.07

--- Ce*s*r*d Strings(7kyu).py
+++ Ce*s*r*d Strings(7kyu).py
@@ -1,9 +1,11 @@
 """
-My PC got infected by a strange virus. It only infects my text files and replaces random letters by *, li*e th*s (like this).
+My PC got infected by a strange virus. It only infects my text files and replaces random letters by *, li*e th*s
+ (like this).
 
 Fortunately, I discovered that the virus hides my censored letters inside root directory.
 
-It will be very tedious to recover all these files manually, so your goal is to implement uncensor function that does the hard work automatically.
+It will be very tedious to recover all these files manually, so your goal is to implement uncensor function that does
+the hard work automatically.
 
 Examples
 uncensor("*h*s *s v*ry *tr*ng*", "Tiiesae") ➜ "This is very strange"
@@ -14,6 +16,7 @@ uncensor("xyz", "") ➜ "xyz"
 """
 
 import codewars_test as test
+
 des = 'Odslpr'
 print(des[1:])
 
@@ -44,8 +47,6 @@ def examples():
 
         for infected, discovered, result in data:
             test.assert_equals(uncensor(infected, discovered), result)
-
-
 
 # Best solution:
 # def uncensor(infected, discovered):
